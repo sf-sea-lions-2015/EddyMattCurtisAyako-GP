3bf3f3f0f80832a1e587dfabb2c23a9f753be491
infin80design & mattymcd <pair+infin80design.mattymcd@devbootcamp.com>
2015-03-12 16:37:29 -0700

    add ascii art database

--- ASCII_Art/db/migrate/20150312105640_create_images.rb
+++ ASCII_Art/db/migrate/20150312105640_create_images.rb
@@ -1,7 +1,8 @@
 class CreateImages < ActiveRecord::Migration
   def change
-    create_image :lists do |t|
+    create_table :images do |t|
       t.string :name
+      t.text :ascii_art
       t.timestamps null: true
     end
   end
--- ASCII_Art/db/seeds.rb
+++ ASCII_Art/db/seeds.rb
@@ -2,9 +2,25 @@
 
 # need to find a way to populate our data-base with ASCII files
 
-# 10.times do
-#   Task.create(:name => Faker::Lorem.words(1..6).join(' '))
-# end
-
-# List.create()
+  Image.create(name: "Bart", ascii_art: "       , ,\ ,'\,'\ ,'\ ,\ ,
+    ,  ;\/ \' `'     `   '  /|
+    |\/                      |
+    :                        |
+    :                        |
+     |                       |
+     |                       |
+     :               -.     _|
+      :                \     `.
+      |         ________:______\\n
+              :,'o       / o    ;
+      :       \       ,'-----./
+       \_      `--.--'        )
+      ,` `.              ,---'|
+      : `                     |
+       `,-'                   |
+       /      ,---.          ,'
+    ,-'            `-,------'
+   '   `.        ,--'
+         `-.____/
+ ")
 
